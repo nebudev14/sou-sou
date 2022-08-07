fdb1f7a9c77278380db00d5dba734f772951292d
warren yun <wyun2006@gmail.com>
2022-08-07 12:34:07 -0400

    map loads

--- components/map.tsx
+++ components/map.tsx
@@ -1,18 +1,56 @@
-import { GoogleMap, Marker, InfoWindow, useJsApiLoader } from "@react-google-maps/api";
+import {
+  GoogleMap,
+  Marker,
+  InfoWindow,
+  useJsApiLoader,
+} from "@react-google-maps/api";
 import { useRef } from "react";
 
-const Map: React.FC = () => {
-  const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_KEY
-  })
+export const MapView: React.FC = () => {
+  const container = {
+    width: "100%",
+    height: "100vh",
+  };
+  
+  const center = {
+    lat: 40.7128,
+    lng: -74
+  };
 
-  const mapRef = useRef<google.maps.Map | null>(null);
+  const options = {
+    disableDefaultUI: true,
+    zoomControl: true
+  }
+
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_KEY,
+  });
+
+  console.log(process.env.REACT_APP_GOOGLE_KEY);
+
+  // const mapRef = useRef<google.maps.Map | null>(null);
+
+  // const onLoad = (map: google.maps.Map): voi
+  // }d => {
+  //   mapRef.current = map;
+
+  // const unMount = (): void => { mapRef.current = null; }
 
   return (
     <>
-      map
+      {isLoaded ? (
+        <GoogleMap
+          mapContainerStyle={container}
+          zoom={12}
+          options={options}
+          center={center}
+          // onLoad={onLoad}
+          // onUnmount={unMount}
+        />
+      ) : (
+        <h1>Loading</h1>
+      )}
     </>
   );
-
-}
+};
