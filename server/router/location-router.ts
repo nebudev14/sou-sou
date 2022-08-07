4c5bf3e6447c7319a2e29299b2fd7bc5b8e211ef
warren yun <wyun2006@gmail.com>
2022-08-07 17:17:14 -0400

    get nearby route

--- server/router/location-router.ts
+++ server/router/location-router.ts
@@ -2,33 +2,54 @@ import axios from "axios";
 import { z } from "zod";
 import { createRouter } from "../create-router";
 
-export const locationRouter = createRouter().mutation("create", {
-  input: z.object({
-    address: z.string(),
-    name: z.string(),
-    groupId: z.string(),
-    lat: z.number(),
-    lng: z.number()
-  }),
-  async resolve({ input, ctx }) {
-    fetch(
-      `https://maps.googleapis.com/maps/api/geocode/json?address=${input.address}&key=${process.env.REACT_APP_GOOGLE_KEY}`
-    )
-    .then((response) => {
-      return response.json();
-    }).then(data => {
-      return prisma.location.create({
-        data: {
-          groupId: input.groupId,
-          address: input.address,
-          name: input.name,
-          lat: input.lat,
-          lng: input.lng,
-        }
-      })
-    })
-    .catch(err => {
-      console.log(err);
-    });
-  },
-});
+export const locationRouter = createRouter()
+  .mutation("create", {
+    input: z.object({
+      address: z.string(),
+      name: z.string(),
+      groupId: z.string(),
+      lat: z.number(),
+      lng: z.number(),
+    }),
+    async resolve({ input, ctx }) {
+      fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${input.address}&key=${process.env.REACT_APP_GOOGLE_KEY}`
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          return prisma.location.create({
+            data: {
+              groupId: input.groupId,
+              address: input.address,
+              name: input.name,
+              lat: input.lat,
+              lng: input.lng,
+            },
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  })
+  .query("get-nearby", {
+    input: z.object({
+      address: z.string(),
+    }),
+    async resolve({ input, ctx }) {
+      fetch(
+        `https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${process.env.REACT_APP_GOOGLE_KEY}&location=,&radius=90`
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          return data.results;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  });
