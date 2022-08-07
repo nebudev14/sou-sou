6a89f0b798befc77ce888709c87053de27cf435c
warren yun <wyun2006@gmail.com>
2022-08-07 12:34:13 -0400

    fix env

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
+require('dotenv').config();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    REACT_APP_GOOGLE_KEY: process.env.REACT_APP_GOOGLE_KEY
+  }
 }
 
 module.exports = nextConfig
--- pages/map.tsx
+++ pages/map.tsx
@@ -1,10 +1,11 @@
 import { NextPage } from "next";
+import { MapView } from "../components/map";
 
 const Map: NextPage = () => {
   return (
-    <div className="h-screen grid grid-cols-2">
+    <div className="grid h-screen grid-cols-2">
       <div className="text-center border-r-4 border-white">
-        WEEEEE
+        <MapView />
       </div>
       <div className="text-center border-r-4 border-white">
         WOOOOO
