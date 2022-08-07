28a339e06cbadf999db80650ad1a1297efbb5651
warren yun <wyun2006@gmail.com>
2022-08-07 16:49:07 -0400

    search button

--- components/search.tsx
+++ components/search.tsx
@@ -1,7 +1,15 @@
+import { AiOutlineSearch } from "react-icons/ai";
+
 export const Search: React.FC = () => {
   return (
-    <input className="absolute z-50 px-3 py-3 mt-8 text-xl border border-gray-300 rounded-lg shadow-xl outline-none bg-slate-50">
-      
-    </input>
+    <div className="absolute z-50 flex flex-row items-center justify-center mt-8">
+      <input
+        className="p-3 text-xl border border-gray-300 rounded-l-lg shadow-xl outline-none bg-slate-50"
+        autoComplete="off"
+      />
+      <button className="h-full p-3 text-2xl text-white bg-pink-600 rounded-r-lg shadow-xl">
+        <AiOutlineSearch />
+      </button>
+    </div>
   );
-}
+};
