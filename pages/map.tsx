61417491a4a8f7d51c02627ce46d67231453e716
warren yun <wyun2006@gmail.com>
2022-08-06 21:51:45 -0400

    init prisma

---
+++ pages/map.tsx
@@ -0,0 +1,16 @@
+import { NextPage } from "next";
+
+const Map: NextPage = () => {
+  return (
+    <div className="h-screen grid grid-cols-2">
+      <div className="text-center border-r-4 border-white">
+        WEEEEE
+      </div>
+      <div className="text-center border-r-4 border-white">
+        WOOOOO
+      </div>
+    </div>
+  );
+}
+
+export default Map;
