43808f765ded1b3c830ca9e3944018463fbff69f
kea <kea@bxscience.edu>
2022-08-07 21:48:54 -0400

    added new welecome message to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,8 +17,10 @@ const Home: NextPage = () => {
     <div className="flex flex-col items-center justify-center h-screen bg-sage-blue">
       {status === "unauthenticated" ? (
         <>
+          <h1 className="text-5xl text-golden-yellow">welcome to sou-sou!</h1>
+          <p className="text-cream"> where you'll find "all the places you'll go"</p>
           <button
-            className="px-4 py-3 mb-4 text-2xl text-sage-blue duration-200 bg-baby-pink rounded-xl hover:bg-light-coral hover:text-cream"
+            className="px-4 py-3 mb-4 mt-6 text-xl text-sage-blue duration-200 bg-baby-pink rounded-xl hover:bg-light-coral hover:text-cream"
             onClick={() => signIn("google")}
           >
             Sign Up!
@@ -26,10 +28,11 @@ const Home: NextPage = () => {
         </>
       ) : (
         <>
-          <h1 className="text-2xl">Welcome, {session?.user?.name}</h1>
+          <h1 className="text-5xl text-golden-yellow">{session?.user?.name}, welcome to the party!</h1>
+          <p className="text-cream"> where you'll find "all the places you'll go"</p>
           <Link href="/map" className="text-gray-900 hover:text-gray-900 focus:text-gray-900 my-1.5">
             <button
-                className="px-4 py-3 my-4 text-2xl text-sage-blue duration-200 bg-baby-pink rounded-xl hover:bg-light-coral hover:text-cream"
+                className="px-4 py-3 mt-6 text-xl text-sage-blue duration-200 bg-baby-pink rounded-xl hover:bg-light-coral hover:text-cream"
               >
                 Get Started!
               </button>
--- pages/map.tsx
+++ pages/map.tsx
@@ -4,10 +4,15 @@ import { MapView } from "../components/map";
 import { Search } from "../components/search";
 import { useQuery } from "../hooks/trpc";
 import { useAtom } from "jotai";
-import { addPeopleAtom, createGroupAtom, selectedGroupAtom } from "../server/atoms";
+import {
+  addPeopleAtom,
+  createGroupAtom,
+  selectedGroupAtom,
+} from "../server/atoms";
 import { CreateGroupModal } from "../components/modals/create-group-modal";
 import { BsCheckLg } from "react-icons/Bs";
 import { AddUserModal } from "../components/modals/add-people";
+import { User, Location } from "@prisma/client";
 
 const Map: NextPage = () => {
   const { data: userData } = useSession();
@@ -18,9 +23,9 @@ const Map: NextPage = () => {
   ]);
 
   const [groupIsOpen, setGroupIsOpen] = useAtom(createGroupAtom);
-  const [, setPeopleIsOpen] = useAtom(addPeopleAtom);  
+  const [, setPeopleIsOpen] = useAtom(addPeopleAtom);
   const [selectedGroup, setSelectedGroup] = useAtom(selectedGroupAtom);
-  console.log(selectedGroup)
+  console.log(selectedGroup);
 
   return (
     <div className="grid h-screen grid-cols-2 overflow-hidden">
@@ -61,13 +66,6 @@ const Map: NextPage = () => {
                 </div>
               ))}
             </div>
-<<<<<<< HEAD
-            <div>
-              <h1 className="mb-4 text-golden-yellow text-2xl p-2.5 border-b-2 border-cream">PEOPLE</h1>
-              {groupData?.map((user, i) => (
-                <h1 className="text-p-2 text-4xl text-cream border-b-2 border-cream hover:bg-baby-pink hover:text-sage-blue" 
-                key={i}>{user.name}</h1>
-=======
 
             <div className="mb-4 border-r-2 border-cream ">
               <div className="flex flex-row items-center justify-center border-b-2 border-cream">
@@ -81,7 +79,7 @@ const Map: NextPage = () => {
                   +
                 </button>
               </div>
-              {selectedGroup?.users?.map((user, i) => (
+              {selectedGroup?.users?.map((user: User, i: any) => (
                 <div
                   key={i}
                   className="flex items-center justify-center py-4 text-2xl duration-200 border-b-2 text-p-2 text-cream border-cream hover:cursor-pointer hover:text-sage-blue hover:bg-baby-pink"
@@ -89,14 +87,15 @@ const Map: NextPage = () => {
                   <h1 className="mr-6" key={i}>
                     {user?.name}
                   </h1>
-
                 </div>
->>>>>>> 9fa35b7a5370b94bd5e4b382337eef1c9673b7ca
               ))}
             </div>
           </div>
           <div>
-            <h1 className="mt-8 text-2xl text-golden-yellow">PLACES</h1>
+            <h1 className="mt-8 mb-6 text-2xl text-golden-yellow">PLACES</h1>
+            <div className="flex items-center justify-start">
+              
+            </div>
           </div>
         </div>
       </div>
